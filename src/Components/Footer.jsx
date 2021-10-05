6c7e849495ec1aa115284a36841bdbf3ee3a3c48
Tomas Capizzi <tomas.capizzi@hotmail.com>
2021-10-04 21:57:11 -0300

    Update Footer.jsx

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -5,9 +5,9 @@ export default function Footer(){
 
     return(
         <div className='footer'>
-            <p>This app was made using <span><a href="http://worldtimeapi.org/" rel='no-referrer' target='_blank'>worldtimeapi</a></span> by Tomas Capizzi</p>
-            <a href="https://www.linkedin.com/in/tomas-capizzi/" rel='no-referrer' target='_blank'><AiFillLinkedin/></a>
-            <a href="https://github.com/TomasCapizzi" rel='no-referrer' target='_blank'><AiFillGithub/></a>
+            <p>This app was made using <span><a href="http://worldtimeapi.org/" rel='noreferrer' target='_blank'>worldtimeapi</a></span> by Tomas Capizzi</p>
+            <a href="https://www.linkedin.com/in/tomas-capizzi/" rel='noreferrer' target='_blank'><AiFillLinkedin/></a>
+            <a href="https://github.com/TomasCapizzi" rel='noreferrer' target='_blank'><AiFillGithub/></a>
         </div>
     )
 }
