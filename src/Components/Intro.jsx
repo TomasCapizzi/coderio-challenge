db68eb4540705ddf3db1461e5c934bf5c218f953
Tomas Capizzi <tomas.capizzi@hotmail.com>
2022-09-24 17:43:57 -0300

    Create Intro.jsx

---
+++ src/Components/Intro.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Intro() {
+  return (
+    <div className='intro'>
+        <h3>Timezones App</h3>
+        <p>Choose between the timezones you want to see. Filter by regions and names!</p>
+    </div>
+  )
+}
+
+export default Intro
