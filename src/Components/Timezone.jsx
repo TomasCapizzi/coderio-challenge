c46b0b844e8a604597258df777fd5618a0e281fc
Tomas Capizzi <tomas.capizzi@hotmail.com>
2022-09-24 17:44:45 -0300

    Create components

--- src/Components/Timezone.jsx
+++ src/Components/Timezone.jsx
@@ -1,17 +1,13 @@
 import React,{useContext} from "react";
-import {MdCancel} from 'react-icons/md'
+
+import Intro from "./Intro";
 import { SelectionsContext } from "../Context/SelectionsContext";
+import Zone from "./Zone";
 
 export default function Timezone(){
 
 
     const {selections, setSelections} = useContext(SelectionsContext);
-
-
-    function removeTimezone(timezone){
-
-        setSelections(selections.filter(item => item.timezone !== timezone))
-    }
 
     return(
         <article className='timezone'>
@@ -21,24 +17,11 @@ export default function Timezone(){
                     {
                      const date = item.datetime.split('T')[0]
                      const time = item.datetime.split('T')[1]   
-                     return <div key={item.timezone}  className='zone'>
-                                <button><MdCancel onClick={ ()=> removeTimezone(item.timezone) }/></button>
-                                <h3>{item.timezone.split('/')[0]}</h3>
-                                {
-                                    item.timezone.split('/')[1] === 'Argentina' ? <h3>{item.timezone.split('/')[2]}</h3>
-                                    :
-                                    <h3>{item.timezone.split('/')[1]}</h3>
-                                }
-                                <p>{date}</p>
-                                <p>{time.split(':')[0] + ' : ' +time.split(':')[1]}</p>
-                            </div>
+                     return <Zone item={item} date={date} time={time} />
                     }
                 )
                 :
-                    <div className='intro'>
-                        <h3>Timezones App</h3>
-                        <p>Choose between the timezones you want to see. Filter by regions and names!</p>
-                    </div>
+                <Intro/>
             }
         </article>
     )
